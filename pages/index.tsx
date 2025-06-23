4246df280503eb8315ef7cad99697b1abfdf0928
bigwavedev <support@bigwavedev.com>
2025-06-23 17:53:45 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <div>Hello from index.tsx</div>;
+import { builder, BuilderComponent } from '@builder.io/react';
+import { GetStaticProps } from 'next';
+
+// Set your Builder.io Public API Key
+builder.init('YOUR_PUBLIC_API_KEY'); // ← we’ll fix this next
+
+export default function Home({ builderContent }: any) {
+  return (
+    <div>
+      {builderContent ? (
+        <BuilderComponent model="page" content={builderContent} />
+      ) : (
+        <div>Coming soon...</div>
+      )}
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const builderContent = await builder
+    .get('page', {
+      userAttributes: {
+        urlPath: '/',
+      },
+    })
+    .toPromise();
+
+  return {
+    props: {
+      builderContent: builderContent || null,
+    },
+    revalidate: 5,
+  };
+};
